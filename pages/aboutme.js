c77adf61a9f272152accdfef0fe6da890a8ae2c1
Shifa Somji <shifamsomji@gmail.com>
2022-11-29 17:18:12 -0800

    updates to about me page

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -31,47 +31,26 @@ export default function AboutMe() {
                             }}
                         >
                             <p>
-                                I'm originally from Seattle, WA and currently attending 
-                                Harvey Mudd College in Claremont, CA (although with the current 
-                                COVID times, I am back in the rainy city of Seattle!). My research interests 
-                                include deep learning analysis of histopathological images and 
-                                predicting the presence of cognitive stress in a patient. 
-                                Recently, I am also interested in thinking about the theoretical 
-                                aspect of machine learning, with more updates coming soon!
-                                I also care about encouraging women to pursue their interests
-                                in STEM and giving back to my community.
+                                I'm originally from Seattle, WA and currently a senior at 
+                                Harvey Mudd College in Claremont, CA. My current research interests include human-computer interaction and more broadly, the application of machine learning to other fields, like computational biology and cognitive science. I am also passionate about encouraging women to pursue their interests in STEM and giving back to my community.
+
+                                Currently, I am applying to graduate programs to pursue my PhD in human-computer interaction and applied machine learning! 
                             </p>
                             <p>
-                                In high school, I founded Chess4Girls, a non-profit organization dedicated to 
-                                closing the gender gap in chess. 
-                                Its mission is to encourage girls to continue playing chess. I did so by hosting
-                                motivational workshops for all participating girls at the 
-                                Washington State Elementary Chess Championship. I also ran a monthly chess 
-                                strategy session for all girls in King County.
-                            </p>
-                            <p>
-                                Other things of interest to me are 
-                                learning how to create successful 
-                                startups, the history of language, 
-                                the intersection of business 
-                                and technology, all genres of music, 
-                                food, chess, and tv shows.
+                                In high school, I founded Chess4Girls, a non-profit organization dedicated to closing the gender gap in chess. 
+                                Its mission is to encourage girls to continue playing chess. I did so by hosting motivational workshops for all participating girls at the Washington State Elementary Chess Championship. I also ran a monthly chess strategy session for all girls in King County.
+
+                                In college, I have served as the secretary for the Society of Women Engineers and the co-chair for the Women's Chapter of the Association of Computing Machinery at the Claremont Colleges. Through both of these experiences, I have engaged women and allies of women in computer science and other STEM fields.
                             </p>
                             <p>
                                 When I'm not constantly struggling with problem sets 
-                                or debugging code, 
-                                you can find me discovering new music,
+                                or debugging code, you can find me discovering new music,
                                 working out, or playing chess.
                             </p>
                             <p>
-                                If you're a student who is building a startup 
-                                or is also interested in the growing field of 
-                                machine learning and artificial intelligence, 
-                                I would absolutely love to hear from you! 
-                                Feel free to
-                                email me at{" "}
-                                <a href="mailto:ssomji@hmc.edu">
-                                    ssomji@hmc.edu
+                                If you're a student who is building a startup or is also interested in the growing field of machine learning and artificial intelligence, I would absolutely love to hear from you! Feel free to email me at{" "}
+                                <a href="mailto:ssomji@g.hmc.edu">
+                                    ssomji@g.hmc.edu
                                 </a>
                                 .
                             </p>
