7e16d76971ec8330d61482bdeccc31ef0b150df2
Shifa Somji <shifamsomji@gmail.com>
2022-12-01 13:45:34 -0800

    edit about me page

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -31,15 +31,15 @@ export default function AboutMe() {
                             }}
                         >
                             <p>
-                                I'm originally from Seattle, WA and currently a senior at 
-                                Harvey Mudd College in Claremont, CA. My current research interests include human-computer interaction and more broadly, the application of machine learning to other fields, like computational biology and cognitive science. I am also passionate about encouraging women to pursue their interests in STEM and giving back to my community.
+                                I'm originally from Seattle, WA and currently a senior computer science major at Harvey Mudd College in Claremont, CA. My current research interests include human-computer interaction and more broadly, the application of machine learning to other fields, like computational biology and cognitive science. I am also passionate about encouraging women to pursue their interests in STEM and giving back to my community.
 
                                 Currently, I am applying to graduate programs to pursue my PhD in human-computer interaction and applied machine learning! 
                             </p>
                             <p>
                                 In high school, I founded Chess4Girls, a non-profit organization dedicated to closing the gender gap in chess. 
                                 Its mission is to encourage girls to continue playing chess. I did so by hosting motivational workshops for all participating girls at the Washington State Elementary Chess Championship. I also ran a monthly chess strategy session for all girls in King County.
-
+                            </p>
+                            <p>
                                 In college, I have served as the secretary for the Society of Women Engineers and the co-chair for the Women's Chapter of the Association of Computing Machinery at the Claremont Colleges. Through both of these experiences, I have engaged women and allies of women in computer science and other STEM fields.
                             </p>
                             <p>
