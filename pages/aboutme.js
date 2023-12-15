db02be47d1df6b50dae6609d460c5069af278324
Shifa Somji <shifamsomji@gmail.com>
2023-12-15 11:57:26 -0800

    Updating about me

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -31,26 +31,18 @@ export default function AboutMe() {
                             }}
                         >
                             <p>
-                                I'm originally from Seattle, WA and currently a senior computer science major at Harvey Mudd College in Claremont, CA. My current research interests include human-computer interaction and more broadly, the application of machine learning to other fields, like computational biology and cognitive science. I am also passionate about encouraging women to pursue their interests in STEM and giving back to my community.
-
-                                Currently, I am applying to graduate programs to pursue my PhD in human-computer interaction and applied machine learning! 
+                                I'm originally from Seattle, WA and currently a first-year PhD student at Purdue. I recently graduated from Harvey Mudd College in May with a B.S. in Computer and Cognitive Science. My current interests lie in full-stack robotics with a focus on enabling robots to interact with the physical world safely, fluently and naturally. I am also passionate about encouraging women to pursue their interests in STEM and giving back to my community.
                             </p>
                             <p>
-                                In high school, I founded Chess4Girls, a non-profit organization dedicated to closing the gender gap in chess. 
-                                Its mission is to encourage girls to continue playing chess. I did so by hosting motivational workshops for all participating girls at the Washington State Elementary Chess Championship. I also ran a monthly chess strategy session for all girls in King County.
+                                I am the CEO of Chess4Girls, a non-profit organization dedicated to closing the gender gap in chess. In college, I served as the secretary for the Society of Women Engineers and the co-chair for the Women's Chapter of the Association of Computing Machinery at the Claremont Colleges. Through both of these experiences, I have engaged women and allies of women in computer science and other STEM fields.
                             </p>
                             <p>
-                                In college, I have served as the secretary for the Society of Women Engineers and the co-chair for the Women's Chapter of the Association of Computing Machinery at the Claremont Colleges. Through both of these experiences, I have engaged women and allies of women in computer science and other STEM fields.
-                            </p>
-                            <p>
-                                When I'm not constantly struggling with problem sets 
-                                or debugging code, you can find me discovering new music,
-                                working out, or playing chess.
+                                In my free time, I enjoy listening to music, reading, and playing chess. Check out my <a href="https://open.spotify.com/user/maycure">Spotify</a> for my playlists and my <a href="https://www.goodreads.com/user/show/166951384-shifa">Goodreads</a> to see what I'm reading currently!
                             </p>
                             <p>
                                 If you're a student who is building a startup or is also interested in the growing field of machine learning and artificial intelligence, I would absolutely love to hear from you! Feel free to email me at{" "}
-                                <a href="mailto:ssomji@g.hmc.edu">
-                                    ssomji@g.hmc.edu
+                                <a href="mailto:ssomji@purdue.edu">
+                                    ssomji@purdue.edu
                                 </a>
                                 .
                             </p>
