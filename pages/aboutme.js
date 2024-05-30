556919d6c8901383b04d16f94c5588d45b701056
Shifa Somji <shifamsomji@gmail.com>
2024-05-29 19:22:53 -0700

    Revert "about me page"
    This reverts commit da73bbf758a5bb2976790ebf67aabaa1a069a407.

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -14,9 +14,9 @@ export default function AboutMe() {
                 <link rel="icon" href="/icon.ico" />
             </Head>
             <div>
+                <p className={shared.titleText}>About Me</p>
                 <NavBar currPage="aboutMe" />
-                <p className={styles.title}>About Me</p>
-                <img 
+                <img
                     src="/Purdue.jpg"
                     alt="purdue photo"
                     className={styles.photo}
@@ -29,7 +29,7 @@ export default function AboutMe() {
                     <div style={{ marginLeft: "2%" }} data-aos="fade-left">
                         <div
                             style={{
-                                marginTop: "2%",
+                                marginTop: "5%",
                                 fontSize: "18px",
                             }}
                         >
